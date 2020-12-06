cf2704aa37ddf26bca4bfcde9f355c2df1f2893b
feji3769 <felix.jimenez@colorado.edu>
2020-12-06 14:07:57 -0600

    filling out tests for simfriedman2 to improve coverage. error checks and set seed tests added. all tests pass.

--- tests/testthat/test-SimFriedman2.R
+++ tests/testthat/test-SimFriedman2.R
@@ -1,13 +1,24 @@
 test_that("SimFriedman2 works", {
+  
+# does the return match expectation?
 N = 100
 sigma = 0.1
 result = SimFriedman2(N, sigma)
 expect_equal(length(result$y), N)
 expect_equal(dim(result$x), c(N, 4))
-  
 
-expect_error(SimFriedman2(2.1, .1))
-expect_error(SimFriedman2(20, -.1))
-expect_error(SimFriedman2(20, 0))
+# does the return match expectation with set seed?
+N = 100
+sigma = 0.1
+result = SimFriedman2(N, sigma, seed = 1)
+expect_equal(length(result$y), N)
+expect_equal(dim(result$x), c(N, 4))
+  
+# do errors get caught?
+expect_error(SimFriedman2(2.1, .1)) # bad N
+expect_error(SimFriedman2(20, -.1)) # bad sigma
+expect_error(SimFriedman2(20, 0)) # bad sigma 
+expect_error(SimFriedman2(20, "0")) # non-numeric input
+
 
 })
