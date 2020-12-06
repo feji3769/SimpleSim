ed393096b093be871fc0ffaccf0fa8bff7bde642
feji3769 <felix.jimenez@colorado.edu>
2020-12-06 14:01:37 -0600

    adding tests for better coverage of SimNormalMix. Added tests to check if errors are caught. all tests pass.

--- tests/testthat/test-SimNormalMix.R
+++ tests/testthat/test-SimNormalMix.R
@@ -11,5 +11,19 @@ test_that("SimNormalMix output is as expected.", {
   expect_equal(length(unique(result$y)), 3) # correct number of classes.
   expect_lt(max(result$x), 4.1) # data range is good?
   expect_gt(min(result$x), -0.1)
+  
+  
+  # do the basics still work when the seed is set?
+  result = SimNormalMix(N, mu, sigma, p, seed = 1)
+  
+  # check data
+  expect_equal(length(result$x), N) # correct number of samples.
+  expect_equal(length(unique(result$y)), 3) # correct number of classes.
+  expect_lt(max(result$x), 4.1) # data range is good?
+  expect_gt(min(result$x), -0.1)
+  
+  expect_error(SimNormalMix(N, mu, sigma, p = p/2))# bad p
+  expect_error(SimNormalMix(N, mu, c(sigma, 1), p = p)) # bad mu sigma pair
+  expect_error(SimNormalMix(1, mu, sigma, p))
 })
 
