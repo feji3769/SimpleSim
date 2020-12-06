e49d5250e8377d075407d8561b5e2476852efab2
feji3769 <felix.jimenez@colorado.edu>
2020-12-06 16:15:51 -0600

    adding more tests and comments for SimBlobs. Code coverage should be  near 100 percent. all tests pass still.

--- R/SimBlobs.R
+++ R/SimBlobs.R
@@ -12,55 +12,64 @@
 #' @export
 #' @examples
 #' N = c(2,2,2)
-#' centers = centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
+#' centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
 #' SimBlobs(N, centers = centers)
+#' 
+#' N = 10
+#' centers = 3
+#' centerBox = c(0,1)
+#' SimBlobs(N, centers = centers, centerBox = centerBox, seed = 1)
 SimBlobs = function(N, nFeatures=3, centers=NULL, centerBox = c(-10,10), seed = NULL){
+ 
   
+  # check the seed is good.  
   if(!is.null(seed)){
     .checkSeed(seed)
     set.seed(seed)
   }
+  
+  # require the bounding box of random centers to be a vector.
   .requireVector(centerBox)
-  if(centerBox[2] < centerBox[1]){
-    stop("Bounding box for centers must be increasing between 1 and 2.")
+  if(centerBox[2] < centerBox[1]){ # bounding box makes sense?
+    stop("Bounding box for centers must be increasing between index 1 and 2.")
   }
-  .checkPositive(N, "N")
+  .checkPositive(N, "N") # make sure N is positive.
   # check the centers input are good. 
   if(is.null(centers)){ # null?
     nCenters = -1 # assign number of centers based on N. 
   } else if(is.matrix(centers)){ # matrix of centers?
-    .requireMatrix(centers, "centers")
-    nCenters = dim(centers)[1] 
-    nFeatures = dim(centers)[2]
+    .requireMatrix(centers, "centers") # centers should be a matrix. 
+    nCenters = dim(centers)[1]  # number of numbers. 
+    nFeatures = dim(centers)[2] # number of featuers. 
   } else { # a single number. 
-    .requireConstant(centers, "centers")
-    nCenters = centers
-    .requireConstant(nFeatures, "nFeatures")
-    .checkPositive(nFeatures, "nFeatures")
-    centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures)
+    .requireConstant(centers, "centers") # require the centers is a singel number. 
+    nCenters = centers # set the  number of centers. 
+    .requireConstant(nFeatures, "nFeatures") # require num features is a constant. 
+    .checkPositive(nFeatures, "nFeatures") # is the number of features positive?
+    centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures) # build centers.
   }
   
   # check the N per groups arg is good. 
-  if(.checkVector(N, "N")){ # Num samples per center.
+  if(.checkVector(N, "N")){ # Is N a vector?
     
-    if(nCenters == -1){
-      nCenters = length(N)
-      centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures)
-    } else if (length(N) != nCenters){
+    if(nCenters == -1){ # assign centers based on N. 
+      nCenters = length(N) # get number of N (ie number of centers.)
+      centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures) # build centesr
+    } else if (length(N) != nCenters){ # stop if there is a mismatch.
       stop("Number of centers must match ")
     }
     
     
-  } else {
-    .requireConstant(N, "N")
-    if(nCenters == -1){
-      nCenters = 8
-      centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures)
-    }
-    N = rep(N, nCenters)
+  } else { # Is N a constant? 
+    .requireConstant(N, "N") # require constant.
+    if(nCenters == -1){ # assign centers based on N.
+      nCenters = 8 # default number of centers. 
+      centers = matrix(runif(nCenters * nFeatures, centerBox[1], centerBox[2]), ncol = nFeatures) # build centers.
+    } 
+    N = rep(N, nCenters) # make vector of N's for C++ call.
     
   }
-  result = SimBlobsC(N, centers)
+  result = SimBlobsC(N, centers) # c++ call
   return(list(x = result$X, y = result$Y, centers = centers))
 }
 
--- tests/testthat/test-SimBlobs.R
+++ tests/testthat/test-SimBlobs.R
@@ -1,15 +1,36 @@
 test_that("Sim blobs works", {
+  
+  # does the core package work?
   N = c(2,2,2)
-  centers = centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
+  centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
   result = SimBlobs(N, centers = centers)
   expect_equal(length(result$y), sum(N))
   
+  # does the core package work with seed set.
+  N = c(2,2,2)
+  centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
+  result = SimBlobs(N, centers = centers, seed = 1)
+  expect_equal(length(result$y), sum(N))
+ 
+  # does the core package work without centers set N a vector?
+  N = c(2,2,2)
+  centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
+  result = SimBlobs(N, seed = 1)
+  expect_equal(length(result$y), sum(N))
   
-  expect_error(SimBlobs(-1))
-  expect_error(SimBlobs(10, -1))
-  expect_error(SimBlobs(10, 3, -1))
+  # does the core package work without centers set N a constant?
+  N = 4
+  centers = matrix(c(1,10,35, 67,6,7), ncol = 2)
+  result = SimBlobs(N, seed = 1)
+  expect_equal(length(result$y), N * 8) # default to 8 centers.
   
-  expect_error(SimBlobs(10, 3, -1))
-  expect_error(SimBlobs(10, centerBox = c(1, 0)))
+  # error checks
+  expect_error(SimBlobs(-1)) # bad N 
+  expect_error(SimBlobs(10, -1)) # bad nFeatures 
+  expect_error(SimBlobs(10, 3, -1)) # bad centers
+  
+  expect_error(SimBlobs(10, 3, -1)) # bad 
+  expect_error(SimBlobs(10, centerBox = c(1, 0))) # bad center box. 
   
 })
+
